0f38453f11fd782605748afc5ec602589407f54a
Amin-Mashayekhan <mashayekhan.mohammadamin@gmail.com>
2022-03-18 08:49:04 +0330

    add modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
+import close from '../assets/images/icon-close.svg';
+import rules from '../assets/images/image-rules.svg';
 
-const Modal = () => {
+
+const Modal = ({toggle}) => {
     return (
-        <div>
-           Modal 
-        </div>
+        ReactDOM.createPortal(
+            <div className="modal-container">
+                <div className="modal-box">
+                    <div className="modal-header">
+                        <h1>Rules</h1>
+                        <button onClick={toggle}>
+                            <img src={close} alt="Close"  />
+                        </button>
+                    </div>
+                    <img src={rules} alt="Rules" />
+                </div>
+            </div>
+            ,
+            document.getElementById("modal")  
+        )
     )
 }
 
 export default Modal
+ 
